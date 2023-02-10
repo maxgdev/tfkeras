76030f0832a19e551d5800bfe6b7ae1ca21d36c2
DESKTOP-84RSE2P\user <max@apphilia.net>
2023-02-10 12:54:28 +0000

    Added sketch.js for ml5 classificaion function

---
+++ sketch.js
@@ -0,0 +1,23 @@
+let classifier;
+let img;
+
+function preload() {
+  classifier = ml5.imageClassifier('MobileNet');
+  img = loadImage('https://www.cdc.gov/healthypets/images/pets/ferret-on-white-background.jpg')
+};
+function setup() {
+  createCanvas(400, 400);
+  classifier.classify(img, gotResult);
+  image(img, 0, 0)
+}
+
+function gotResult(error, results) {
+  if(error) {
+    console.error(error);
+  } else {
+   console.log(results);
+    createDiv(`Label: ${results[0].label}`);
+    createDiv(`Confidence: ${nf(results[0].confidence, 0, 2)}`)
+  }
+}
+
